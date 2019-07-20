78ba7690ed319fdbefec98c9c313cb0f76650d6e
gaonee <majiancommon@163.com>
2019-07-20 16:29:45 +0800

    Create webpack.common.config.js

---
+++ webpack.common.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    mode: "development",
+    entry: {
+      "bundle": ["./src/index.tsx"],
+      "main": ["./main.tsx"]
+    },
+    output: {
+      filename: "[name].js",
+      path: __dirname + "/dist"
+    },
+  
+    devtool: "source-map",
+  
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+  
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: "ts-loader" },
+  
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+    },
+  
+    plugins: [
+    ],
+
+    target: "electron-renderer"
+};
