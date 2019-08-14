2ffcd71465f8266683480c4d1eb0b285f0c3ae4e
gaonee <majiancommon@163.com>
2019-08-14 15:58:47 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -11,7 +11,7 @@ let createWindow = function() {
       // fullscreenable:false,
       // maximizable:false
     })
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
     mainWindow.loadFile('index.html')
     mainWindow.on('closed', function() {
         mainWindow = null
