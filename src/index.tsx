65007a5148919210d9a7af35e50879b7e1f07923
gaonee <majiancommon@163.com>
2019-08-14 16:08:30 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import Hello from "./components/Hello";
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+
+import reducer from './reducers'; 
+
+import Footer from './components/Footer'
+import AddTodo from './container/AddTodo'
+import VisibleTodoList from './container/VisibleTodoList'
+
+const App = () => {
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+        </div>
+    )
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
-  document.getElementById('root') as HTMLElement
+    <Provider store={store}>
+        <App></App>
+    </Provider>,
+    document.getElementById('root') as HTMLElement
 );
